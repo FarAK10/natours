fe6623401ff6bb9c0b2a5a7fd9b133bd64cb0517
FarAK10 <botirovfarhod115@gmail.com>
2023-03-30 15:20:46 +0500

    feat:add embedding

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -99,7 +100,8 @@ const tourSchema = new mongoose.Schema(
         description: String,
         day: Number
       }
-    ]
+    ],
+    guides: Array
   },
   {
     toJSON: { virtuals: true },
@@ -114,6 +116,15 @@ tourSchema.virtual('durationWeeks').get(function() {
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function(next) {
+  const guidesPromises = this.guides.map(async id => {
+    const guide = await User.findById(id);
+    return guide;
+  });
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
