9bae55f715a1604d128df3747dd4eeb2f3b728d0
Farkhod Botirov <botirovfarhod115@gmail.com>
2023-05-11 12:57:46 +0500

    fix: allow cors

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: '*',
+    origin: ['http://localhost:3000', 'https://farak10-natours.onrender.com/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true
   })
